3fdb98728655a76494ce750eb20970c9ebe2bbbf
a01045838844 <a01045838844@gmail.com>
2025-09-15 23:18:46 +0900

    add root and webhook route

--- tv_webhook_server.py
+++ tv_webhook_server.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+# 서버 살아있는지 확인용
 @app.get("/")
 async def root():
     return {"status": "server is live"}
 
+# 웹훅 엔드포인트
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
